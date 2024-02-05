29c854645aa2f41863ebb14136e2e092056f7597
ksjung <ksjung@bitmango.com>
2024-02-05 23:01:07 +0900

    Add item endpoint

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
+from typing import Union
+
 from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
 #main code
-@app.get("/get")
-async def get_data(q):
-    ...
-    return q
+@app.get("/")
+async def get_data():
+    return {"this is open-api.yangarch.net"}
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
 
 @app.post("/post")
 async def post_data(request: Request):
